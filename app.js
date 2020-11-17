cd71d0161fc96d35a7d7e261e9f34070f0960784
Saqib <saqib29111@gmail.com>
2020-11-17 20:09:01 +0600

    initial environment setup for the the project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express           = require('express');
+const expressSession    = require('express-session');
+const bodyParser        = require('body-parser');
+
+const app               = express();
+const port              = process.env.port || 3000;
+
+// configaration
+app.set('view engine', 'ejs');
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession({ secret: 'secrete value', saveUninitialized: true, resave: false }));
+
+
+app.listen(port, (err)=>{
+    console.log(`Server started at ${port}`);
+});
