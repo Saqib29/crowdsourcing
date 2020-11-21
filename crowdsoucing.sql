3aabee83c07fa2bd703dcc7a368d1227e399a298
Saqib <saqib29111@gmail.com>
2020-11-22 03:51:10 +0600

    updated database added

--- crowdsoucing.sql
+++ crowdsoucing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2020 at 11:06 AM
+-- Generation Time: Nov 21, 2020 at 06:55 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `crowdsourcing`
+-- Database: `crowdsoucing`
 --
 
 -- --------------------------------------------------------
@@ -44,17 +44,19 @@ CREATE TABLE `message` (
   `name` varchar(100) NOT NULL,
   `receiver` varchar(100) NOT NULL,
   `subject` varchar(500) NOT NULL,
-  `body` varchar(1000) NOT NULL
+  `body` varchar(1000) NOT NULL,
+  `sta` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `message`
 --
 
-INSERT INTO `message` (`id`, `username`, `name`, `receiver`, `subject`, `body`) VALUES
-(1, 'jan', 'Jane Alam', 'fsafdas', '		fasfadsf			', '		sdafasfas			'),
-(2, 'jan', 'Jane Alam', 'gfdgsgt', '		r5tretre			', '		teytbvdgfd			'),
-(3, 'saqib', 'saqib', 'zami', 'ghawrgf', 'aifhasdghf');
+INSERT INTO `message` (`id`, `username`, `name`, `receiver`, `subject`, `body`, `sta`) VALUES
+(1, 'jan', 'Jane Alam', 'fsafdas', '		fasfadsf			', '		sdafasfas			', ''),
+(2, 'jan', 'Jane Alam', 'gfdgsgt', '		r5tretre			', '		teytbvdgfd			', ''),
+(3, 'saqib', 'saqib', 'zami', 'ghawrgf', 'aifhasdghf', ''),
+(4, 'zami', 'Al Zami Arafat', 'ahnaf alam', '		kijykg			', '			fjffj7u		', '');
 
 -- --------------------------------------------------------
 
@@ -74,6 +76,13 @@ CREATE TABLE `post_table` (
   `buyer_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `post_table`
+--
+
+INSERT INTO `post_table` (`id`, `title`, `post_body`, `status`, `buyer_id`, `amount`, `seller_id`, `seller_name`, `buyer_name`) VALUES
+(1, '	 vcb			\r\n					\r\n					', '	; cvlvsdyytkgh		\r\n					\r\n					', 'fadbvgfdutr', '2', 454365476, 0, '', 'Al Zami Arafat');
+
 -- --------------------------------------------------------
 
 --
@@ -82,11 +91,20 @@ CREATE TABLE `post_table` (
 
 CREATE TABLE `seller` (
   `id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `category_name` varchar(100) NOT NULL,
-  `balance` int(11) NOT NULL
+  `balance` int(11) NOT NULL,
+  `project_title` varchar(500) NOT NULL,
+  `buyer_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `seller`
+--
+
+INSERT INTO `seller` (`id`, `user_id`, `name`, `category_name`, `balance`, `project_title`, `buyer_name`) VALUES
+(1, 3, 'ahnaf alam', 'web developer', 1200, 'Hospital Management syestem', 'Al Zami Arafat');
 
 -- --------------------------------------------------------
 
@@ -162,13 +180,19 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `post_table`
+--
+ALTER TABLE `post_table`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `seller`
 --
 ALTER TABLE `seller`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
